6f209b171e9666fd902e3f814b2f26137792a8cc
ErfanGh <erfan.ghasemian83@gmail.com>
2024-10-09 13:38:33 +0330

    added question objects

---
+++ s5.js
@@ -0,0 +1,26 @@
+let exampleQuestions = [
+    {
+        'question' : 'بزرگترین سیاره منظومه شمسی کدام است ؟',
+        'option1' : 'زحل',
+        'option2' : 'مریخ',
+        'option3' : 'مشتری',
+        'option4' : 'زمین',
+        'answer' : 'مشتری'
+},
+{
+    'question' : 'پایتخت ایران کجاست ؟',
+    'option1' : 'شیراز',
+    'option2' : 'تهران',
+    'option3' : 'اصفهان',
+    'option4' : 'تبریز',
+    'answer' : 'تهران'
+},
+{
+    'question' : 'تعداد روز های سال کبیسه چنداست ؟',
+    'option1' : '365',
+    'option2' : '366',
+    'option3' : '364',
+    'option4' : '367',
+    'answer' : '365'
+}
+]
